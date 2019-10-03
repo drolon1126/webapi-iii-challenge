f4b31cd03d10542c50ec6526eac9346f4cef9652
danRolon <daniel.rolon@yahoo.com>
2019-10-03 17:03:34 -0500

    setting up for heroku

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
 const server = require('./server.js');
 
-server.listen(5000, () => console.log('API running on port 5000'));
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => console.log(`API running on port ${port}`));
